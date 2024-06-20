da47ca9b93801505f31a2fb46ff9f9404bb6d816
Fellipe <fellipe.silveira.silva@gmail.com>
2024-06-20 18:42:24 -0300

    Department update implemented

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,24 +14,34 @@ public class Program2 {
 		Department dep;
 		Scanner sc = new Scanner(System.in);
 		
-		/*System.out.println("==== TESTE 1: findById ====");
+		/*System.out.println("==== TESTE 1: Department findById ====");
 		System.out.println("Enter an Id to find Department");
 		int id = sc.nextInt();
 		sc.nextLine();
 		dep = depDao.findById(id);
 		System.out.println("Resultado da busca:\n" + dep);*/
 		
-		/*System.out.println("==== TESTE 2: Insert ====");
+		/*System.out.println("==== TESTE 2: Department Insert ====");
 		System.out.println("Digite o nome do departamento que deseja inserir");
 		dep = new Department(null, sc.nextLine());
 		depDao.insert(dep);
 		System.out.println("Department " + dep.getName() + " sucessfull created!");*/
 		
-		/*System.out.println("==== TESTE 3: findAll ====");
+		/*System.out.println("==== TESTE 3: Department findAll ====");
 		List<Department> list = depDao.findAll();
 		for(Department obj : list) {
 			System.out.println("Id: " + obj.getId() + " Department: " + obj.getName());
 		}*/
+		
+		/*System.out.println("==== TESTE 4: Department update ====");
+		System.out.println("Insert department id to update");
+		int id = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Insert new department name");
+		String name = sc.nextLine();
+		dep = new Department(id, name);
+		depDao.update(dep);
+		System.out.println("Update complete!");*/
 		
 		sc.close();
 		
